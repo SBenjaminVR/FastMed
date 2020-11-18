04cdf391325ac7c502676112c9436e8a7a870679
fcarrillom <fcm238@gmail.com>
2020-11-18 13:40:19 -0600

    Acepta datos del usuario de los inputs, falta hacer las llamadas con estos datos

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,22 +1,53 @@
 import React from 'react'
 import { Button, TextField } from '@material-ui/core';
 
+var userData = {
+    email: "",
+    password: ""
+}
+
 function LogIn() {
     return (
         <div>
             <h1>LogIn page</h1>
             <div className="login-form">
             <form id="login-form">
-                <TextField id="filled-basic" label="Usuario" variant="filled" className="login-field" />
+                <TextField id="filled-basic" label="Email" variant="filled" className="login-field" name="email" onChange={handleChange} />
                 <br />
-                <TextField id="filled-basic" label="Contraseña" variant="filled" className="login-field"/>
+                <TextField id="filled-basic" label="Contraseña" variant="filled" className="login-field" name="password" onChange={handleChange} />
                 <br /> <br />
-                <Button variant="contained" color="primary" className="login-button">Log in</Button>
+                <Button variant="contained" color="primary" className="login-button" onClick={onClickLogIn}>Entrar</Button>
                 <Button color="primary" className="login-button">Nuevo usuario</Button>
             </form>
         </div>
         </div>
     )
+}
+
+// Update user data from input fields
+function handleChange(e) {
+    const dataField = [e.target.name]
+    userData[dataField] = e.target.value
+}
+
+function onClickLogIn(e) {
+    e.preventDefault();
+    console.log("Login clicked...");
+    
+    if (!checkEmptyFields()) console.log("Empty fields"); // change for alert
+
+    // Call to api
+    // check if user already exists
+    // create new user
+    // confirm
+}
+
+function checkEmptyFields() {
+    var valid = true
+    for (let [key, value] of Object.entries(userData)) {
+        if (value === "") valid = false;
+    }
+    return valid;
 }
 
 export default LogIn;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,25 +1,64 @@
 import React from 'react'
 import { Button, TextField } from '@material-ui/core';
 
+var userData = {
+    firstName: "",
+    lastNames: "",
+    email: "",
+    password: "",
+    re_password: ""
+}
+
 function Register() {
     return (
         <div>
             <h1>Register page</h1>
             <div className="register-form">
                 <form id="register-form">
-                    <TextField id="filled-basic" label="Nombre" variant="filled" className="login-field" />
-                    <TextField id="filled-basic" label="Apellidos" variant="filled" className="login-field" />
+                    <TextField id="filled-basic" label="Nombre" variant="filled" className="login-field" name="firstName" onChange={handleChange} />
+                    <TextField id="filled-basic" label="Apellidos" variant="filled" className="login-field" name="lastNames" onChange={handleChange}/>
                     <br />
-                    <TextField id="filled-basic" label="E-mail" variant="filled" className="login-field" />
+                    <TextField id="filled-basic" label="E-mail" variant="filled" className="login-field" name="email" onChange={handleChange} />
                     <br />
-                    <TextField id="filled-basic" label="Contraseña" variant="filled" className="login-field"/>
-                    <TextField id="filled-basic" label="Confirmar contraseña" variant="filled" className="login-field" />
+                    <TextField id="filled-basic" label="Contraseña" variant="filled" className="login-field" name="password" onChange={handleChange} />
+                    <TextField id="filled-basic" label="Confirmar contraseña" variant="filled" className="login-field" name="re_password" onChange={handleChange}/>
                     <br /><br />
-                    <Button variant="contained" color="primary" className="login-button">Registrar</Button>
+                    <Button type="submit" variant="contained" color="primary" className="login-button" onClick={onClick}>Registrar</Button>
                 </form>
             </div>
         </div>
     )
+}
+
+// Update user data from input fields
+function handleChange(e) {
+    const dataField = [e.target.name]
+    userData[dataField] = e.target.value
+}
+
+function onClick(e) {
+    e.preventDefault();
+    console.log("Register clicked...");
+    
+    if (!checkEmptyFields()) console.log("Empty fields"); // change for alert
+    if (!checkEqualPasswords()) console.log("No same passwords"); // change for alert
+
+    // Call to api
+    // check if user already exists
+    // create new user
+    // confirm
+}
+
+function checkEmptyFields() {
+    var valid = true
+    for (let [key, value] of Object.entries(userData)) {
+        if (value === "") valid = false;
+    }
+    return valid;
+}
+
+function checkEqualPasswords() {
+    return userData["password"] === userData["re_password"];
 }
 
 export default Register;
